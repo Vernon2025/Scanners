9a57d66b5be74f398ade6f74bc4a36aab8a8581e
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-11-15 11:08:48 -0800

    Start Of Scanners

---
+++ MyScanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MyScanner
+{
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner myScanner = new MyScanner();
+        myScanner.run();
+    }
+}

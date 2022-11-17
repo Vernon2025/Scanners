1a7964796cbe69f9e95602d0f542a979f2495cd3
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-11-17 11:44:24 -0800

    Second

--- MyScanner.java
+++ MyScanner.java
@@ -2,16 +2,50 @@ import java.util.Scanner;
 
 public class MyScanner
 {
-
     public void run() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-
-        }
+       Scanner scanner = new Scanner(System.in);
+       while (true) {
+           if (scanner.hasNextLine()) {
+               String user_input = scanner.nextLine();
+               process_user_input(user_input);
+               string_user_input(user_input);
+           }
+       }
     }
-
-    public static void main(String[] args) {
-        MyScanner myScanner = new MyScanner();
-        myScanner.run();
+    
+    
+    
+    public void process_user_input(String user_input) {
+       Scanner int_scanner = new Scanner(user_input);
+       if (int_scanner.hasNextInt()) {
+           System.out.println("Here is the list of integers:");
+           while (int_scanner.hasNextInt()) {
+               int int_input = int_scanner.nextInt();
+               System.out.println(int_input * 3);
+           }
+           System.out.println();
+       }
     }
+    
+    
+    
+    public void string_user_input(String user_input) {
+        Scanner str_scanner = new Scanner(user_input);
+        if (str_scanner.hasNext()) {
+            System.out.println("List of words:");
+            while (str_scanner.hasNext()) {
+                 String str_input = str_scanner.next();
+                 System.out.println(str_input);
+            }
+            System.out.println();
+        } 
+    }
+    
+        public static void main(String[] args) {
+       MyScanner myScanner = new MyScanner();
+       myScanner.run();
+    }    
 }
+    
+    
+    

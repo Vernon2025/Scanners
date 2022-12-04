c6828c7567f181dfcf8ce6e61f9c3bef7dc85f74
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-12-04 13:22:52 -0800

    In progress

--- MyScanner.java
+++ MyScanner.java
@@ -1,51 +1,56 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MyScanner
 {
+    public static void main(String[] args) {
+        MyScanner myScanner = new MyScanner();
+        myScanner.run();
+    }
+    @SuppressWarnings("InfiniteLoopStatement")
     public void run() {
-       Scanner scanner = new Scanner(System.in);
-       while (true) {
-           if (scanner.hasNextLine()) {
-               String user_input = scanner.nextLine();
-               process_user_input(user_input);
-               string_user_input(user_input);
-           }
-       }
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            if (scanner.hasNextLine()) {
+                String scan_input = scanner.nextLine();
+                process_user_input(scan_input);
+            }
+        }
     }
-    
-    
-    
-    public void process_user_input(String user_input) {
-       Scanner int_scanner = new Scanner(user_input);
-       if (int_scanner.hasNextInt()) {
-           System.out.println("Here is the list of integers:");
-           while (int_scanner.hasNextInt()) {
-               int int_input = int_scanner.nextInt();
-               System.out.println(int_input * 3);
-           }
-           System.out.println();
-       }
-    }
-    
-    
-    
-    public void string_user_input(String user_input) {
-        Scanner str_scanner = new Scanner(user_input);
-        if (str_scanner.hasNext()) {
-            System.out.println("List of words:");
-            while (str_scanner.hasNext()) {
-                 String str_input = str_scanner.next();
-                 System.out.println(str_input);
+    public void process_user_input(String scan_input) {
+        ArrayList<Integer> intList = new ArrayList<>();
+        ArrayList<String> strList = new ArrayList<>();
+        Scanner scanner = new Scanner(scan_input);
+        if (scan_input.contains(",")) {
+            scanner.useDelimiter(",");
+        }
+        if (scanner.hasNextInt()) {
+            while (scanner.hasNextInt()) {
+                intList.add(scanner.nextInt());
+            }
+        } else {
+            while (scanner.hasNext()) {
+                strList.add(scanner.next());
+            }
+        }
+        if (strList.size() > 0) {
+            System.out.println("Here is a list of strings:");
+            for (String s : strList) {
+                System.out.println(s.trim());
+            }
+        } else if (intList.size() > 0) {
+            System.out.println("Here is a list of integers:");
+            for (Integer i : intList) {
+                System.out.println(i);
             }
             System.out.println();
-        } 
+            System.out.println("Here is the list of integers multiplied by 3:");
+            for (Integer i : intList) {
+                System.out.println(i*3);
+            }
+        } else {
+            System.out.println("ERROR: No words or integers detected!!");
+        }
+        System.out.println();
     }
-    
-        public static void main(String[] args) {
-       MyScanner myScanner = new MyScanner();
-       myScanner.run();
-    }    
 }
-    
-    
-    
